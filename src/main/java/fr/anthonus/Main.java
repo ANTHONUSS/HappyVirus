ebadf91ef2172abf96e63896c539998e5b076c75
ANTHONUS <anthonus.d@gmail.com>
2025-04-18 01:55:36 +0200

    bah à peu près tout ducoup

---
+++ src/main/java/fr/anthonus/Main.java
@@ -0,0 +1,20 @@
+package fr.anthonus;
+
+import fr.anthonus.runs.RandomImageSound;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Random rand = new Random();
+        while (true) {
+            int randInt = rand.nextInt(60000, 1200000);
+            System.out.println("Waiting for " + randInt/1000 + "s");
+            Thread.sleep(randInt);
+            switch (rand.nextInt(2)){
+                case 0 -> new RandomImageSound("/fr/anthonus/images/goku.png", "/fr/anthonus/audio/goku.wav");
+                case 1 -> new RandomImageSound("/fr/anthonus/images/gyatt.png", "/fr/anthonus/audio/gyatt.wav");
+            }
+        }
+    }
+}
