d0fa1028ad6a9f343d3feac38141cff3d1c9da05
ANTHONUSS <anthonus.d@gmail.com>
2025-08-12 17:12:30 +0200

    ajout des elements de base pour le projet

--- src/main/java/fr/anthonus/Main.java
+++ src/main/java/fr/anthonus/Main.java
@@ -1,7 +1,31 @@
 package fr.anthonus;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.net.URISyntaxException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws URISyntaxException {
+        Pane root = new Pane();
+
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setMaximized(true);
+        stage.setAlwaysOnTop(true);
+
+        Scene scene = new Scene(root, Color.TRANSPARENT);
+        stage.setScene(scene);
+
+        stage.show();
+    }
+
     public static void main(String[] args)  {
+        launch(args);
     }
 
 }
---
+++ src/main/java/fr/anthonus/utils/Utils.java
@@ -0,0 +1,45 @@
+package fr.anthonus.utils;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.net.URL;
+
+public class Utils {
+
+    public static ImageView createImage(String path, double x, double y, double w, double h, double o) {
+        String url;
+        File f = new File(path);
+        if (f.exists()) {
+            url = f.toURI().toString();
+        } else {
+            URL res = Utils.class.getResource(path.startsWith("/") ? path : "/" + path);
+            if (res != null) {
+                url = res.toExternalForm();
+            } else {
+                throw new IllegalArgumentException("Image non trouvée : " + path);
+            }
+        }
+
+        double requiredWidth = (w > 0) ? w : 0;
+        double requiredHeight = (h > 0) ? h : 0;
+        Image img = new Image(url, requiredWidth, requiredHeight, true, true, true);
+
+        ImageView imageView = new ImageView(img);
+
+        if (w > 0) imageView.setFitWidth(w);
+        if (h > 0) imageView.setFitHeight(h);
+        imageView.setPreserveRatio(true);
+        imageView.setOpacity(o);
+        imageView.setSmooth(true);
+        imageView.setCache(true);
+
+        imageView.setLayoutX(x);
+        imageView.setLayoutY(y);
+
+        imageView.setPickOnBounds(true);
+
+        return imageView;
+    }
+}
